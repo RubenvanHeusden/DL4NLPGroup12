3445269c2ec1ff6ceee134a75e262bdd5c9a5fbf
DeadFireNL <danielgroen96@hotmail.com>
2019-09-27 22:07:34 +0200

    Added full GPU support

--- models/simplelstm.py
+++ models/simplelstm.py
@@ -2,23 +2,25 @@ import torch
 from torch import nn
 
 class SimpleLSTM(nn.Module):
-    def __init__(self, hidden_dim, vocab_size, embedding_dim=300,batch_size=64,weight_matrix=None):
+    def __init__(self, hidden_dim, vocab_size, embedding_dim=300,batch_size=64,weight_matrix=None,dvc=None,tr_embed=False):
         super(SimpleLSTM, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.embedding.load_state_dict({'weight':weight_matrix})
+        self.embedding.weight.requires_grad = tr_embed
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.output_layer = nn.Linear(hidden_dim, 90)
         self.hidden_dim = hidden_dim
         self.vocab_size = vocab_size
-        self.drop = nn.Dropout(0.3)
+        self.drop = nn.Dropout(0.4)
         self.embedding_dim = embedding_dim
         self.batch_size = batch_size
+        self.device = dvc
 
     def forward(self, x):
         x = self.embedding(x)
         # maybe zeros or xavier / normal distribution
-        h_0 = torch.zeros(1, x.size()[1], self.hidden_dim)
-        c_0 = torch.zeros(1, x.size()[1], self.hidden_dim)
+        h_0 = torch.zeros(1, x.size()[1], self.hidden_dim).to(self.device)
+        c_0 = torch.zeros(1, x.size()[1], self.hidden_dim).to(self.device)
         h, _ = self.lstm(x, (h_0, c_0))
         h = self.drop(h)[-1]
         out = self.output_layer(h)
